155eb39567b945e1d3b83f0f728761fba7b1ff4e
rutvijdave12 <rutvijdave12@gmail.com>
2024-02-12 09:32:19 -0500

    Initial Commit

---
+++ Desserts/DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsApp.swift
+//  Desserts
+//
+//  Created by Rutvij Dave on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DessertsUITests/DessertsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DessertsUITestsLaunchTests.swift
+//  DessertsUITests
+//
+//  Created by Rutvij Dave on 2/12/24.
+//
+
+import XCTest
+
+final class DessertsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
